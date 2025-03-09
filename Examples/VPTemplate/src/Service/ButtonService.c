c719b852c668a3a34295eecda549ba0c8f2da2b2
Lukas Reil <lukas.reil0305@gmx.net>
2025-03-09 12:18:38 +0100

    Updated the Button Service

--- Examples/VPTemplate/src/Service/ButtonService.c
+++ Examples/VPTemplate/src/Service/ButtonService.c
@@ -40,77 +40,87 @@ void hysteresis(uint8_t input, uint8_t* lastInputs, uint8_t* output, uint8_t *ou
 
 /***** PUBLIC FUNCTIONS ******************************************************/
 
-uint8_t g_buttonB1Value;
-uint8_t g_buttonB1ChangeRead;
-uint8_t g_buttonSW1Value;
-uint8_t g_buttonSW1ChangeRead;
-uint8_t g_buttonSW2Value;
-uint8_t g_buttonSW2ChangeRead;
+static uint8_t s_buttonB1Value;
+static uint8_t s_buttonB1ChangeRead;
+static uint8_t s_buttonSW1Value;
+static uint8_t s_buttonSW1ChangeRead;
+static uint8_t s_buttonSW2Value;
+static uint8_t s_buttonSW2ChangeRead;
+
+void initButtonService()
+{
+    for(uint8_t i = 0; i < BUTTON_FILTER_WINDOW_SIZE; i++)
+    {
+        readButtonB1();
+        readButtonSW1();
+        readButtonSW2();
+    }
+}
 
 uint8_t wasButtonB1Pressed()
 {
-    if(g_buttonB1ChangeRead)
+    if(s_buttonB1ChangeRead)
     {
         return 0;
     }
-    g_buttonB1ChangeRead = 1;
-    return g_buttonB1Value;
+    s_buttonB1ChangeRead = 1;
+    return s_buttonB1Value;
 }
 
 uint8_t wasButtonSW1Pressed()
 {
-    if(g_buttonSW1ChangeRead)
+    if(s_buttonSW1ChangeRead)
     {
         return 0;
     }
-    g_buttonSW1ChangeRead = 1;
-    return g_buttonSW1Value;
+    s_buttonSW1ChangeRead = 1;
+    return s_buttonSW1Value;
 }
 
 uint8_t wasButtonSW2Pressed()
 {
-    if(g_buttonSW2ChangeRead)
+    if(s_buttonSW2ChangeRead)
     {
         return 0;
     }
-    g_buttonSW2ChangeRead = 1;
-    return g_buttonSW2Value;
+    s_buttonSW2ChangeRead = 1;
+    return s_buttonSW2Value;
 }
 
 uint8_t getButtonB1Value()
 {
-    return g_buttonB1Value;
+    return s_buttonB1Value;
 }
 
 uint8_t getButtonSW1Value()
 {
-    return g_buttonSW1Value;
+    return s_buttonSW1Value;
 }
 
 uint8_t getButtonSW2Value()
 {
-    return g_buttonSW2Value;
+    return s_buttonSW2Value;
 }
 
 void readButtonB1()
 {
     static uint8_t lastInputs[BUTTON_FILTER_WINDOW_SIZE];
     uint8_t buttonValue = buttonGetButtonStatus(BTN_B1) == BUTTON_PRESSED;
-    hysteresis(buttonValue, lastInputs, &g_buttonB1Value, &g_buttonB1ChangeRead);
+    hysteresis(buttonValue, lastInputs, &s_buttonB1Value, &s_buttonB1ChangeRead);
 }
 
 void readButtonSW1()
 {
     static uint8_t lastInputs[BUTTON_FILTER_WINDOW_SIZE];
     uint8_t buttonValue = buttonGetButtonStatus(BTN_SW1) == BUTTON_PRESSED;
-    hysteresis(buttonValue, lastInputs, &g_buttonSW1Value, &g_buttonSW1ChangeRead);
+    hysteresis(buttonValue, lastInputs, &s_buttonSW1Value, &s_buttonSW1ChangeRead);
 }
 
 void readButtonSW2()
 {
     static uint8_t lastInputs[BUTTON_FILTER_WINDOW_SIZE];
     uint8_t buttonValue = buttonGetButtonStatus(BTN_SW2) == BUTTON_PRESSED;
-    hysteresis(buttonValue, lastInputs, &g_buttonSW2Value, &g_buttonSW2ChangeRead);
+    hysteresis(buttonValue, lastInputs, &s_buttonSW2Value, &s_buttonSW2ChangeRead);
 }
 
 /***** PRIVATE FUNCTIONS *****************************************************/
--- Examples/VPTemplate/src/Service/ButtonService.h
+++ Examples/VPTemplate/src/Service/ButtonService.h
@@ -35,6 +35,11 @@
 /***** PROTOTYPES ************************************************************/
 
 /**
+ * @brief Initializes the Button Service
+ */
+void initButtonService();
+
+/**
  * @return 1 if the button B1 was pressed and this is the first time the function is called
  *         0 otherwise
  */
@@ -69,19 +74,16 @@ uint8_t getButtonSW2Value();
 
 /**
  * @brief   Reads the value of B1 and debounces it using a two-point controller
- *          The filtered value is stored in the global variable g_buttonB1Value
  */
 void readButtonB1();
 
 /**
  * @brief   Reads the value of SW1 and debounces it using a two-point controller
- *          The filtered value is stored in the global variable g_buttonSW1Value
  */
 void readButtonSW1();
 
 /**
  * @brief   Reads the value of SW2 and debounces it using a two-point controller
- *          The filtered value is stored in the global variable g_buttonSW2Value
  */
 void readButtonSW2();
 
